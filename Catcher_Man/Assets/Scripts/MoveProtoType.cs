09617fba948d2de540a3154cb4b39de06ac926ac
Rams-Us <0rg3261c0y2064j@au.com>
2026-08-14 00:13:19 +0900

    仮ステージ配置と、InputSystemによるキャラクターの操作の追加

---
+++ Catcher_Man/Assets/Scripts/MoveProtoType.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveProtoType : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpPower = 7f;
+
+    private Rigidbody rb;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private float moveInput;
+    private bool jumpRequested;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    private void OnEnable()
+    {
+        moveAction.performed += OnMove;
+        moveAction.canceled += OnMoveCanceled;
+        jumpAction.started += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.performed -= OnMove;
+        moveAction.canceled -= OnMoveCanceled;
+        jumpAction.started -= OnJump;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (jumpRequested)
+        {
+            Jump();
+            jumpRequested = false;
+        }
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+
+        // 入力上の左右方向を、ワールド座標のZ軸に使う
+        moveInput = input.x;
+        
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = 0f;
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        // 入力イベントでは要求だけを記録する
+        jumpRequested = true;
+    }
+
+    private void Move()
+    {
+        Vector3 velocity = rb.linearVelocity;
+
+        velocity.z = moveInput * moveSpeed;
+
+        // X方向には移動させない
+        velocity.x = 0f;
+
+        rb.linearVelocity = velocity;
+    }
+
+    private void Jump()
+    {
+        Vector3 velocity = rb.linearVelocity;
+        velocity.y = jumpPower;
+        rb.linearVelocity = velocity;
+    }
+}
